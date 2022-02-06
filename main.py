e79727fba5f13fa54dcb1b217942531ce0c953e2
fra <francesco.zuppichini@gmail.com>
2022-02-06 16:31:59 +0100

    minor changes

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import os
 from nbconvert import MarkdownExporter
 
 def main():
-    nb_filename = os.environ.get("INPUT_NB_FILENAME", 'notebook.ipynb')
-    md_filename = os.environ.get("INPUT_MD_FILENAME", 'README.md')
+    work_dir = os.environ.get('GITHUB_WORKSPACE', '.')
+    nb_filename = os.path.join(work_dir, 
+        os.environ.get("INPUT_NB_FILENAME", 'notebook.ipynb')
+    )
+    md_filename = os.path.join(work_dir, os.environ.get("INPUT_MD_FILENAME", 'README.md'))
 
-    print(f"::debug::Converting {nb_filename}")
+    # print(f"::debug::Converting {nb_filename}")
 
-    md_exporter = MarkdownExporter()
-    # Convert the notebook to RST format
-    (body, resources) = md_exporter.from_filename(nb_filename)
-    print(f"::debug::Saving{md_filename}")
+    # md_exporter = MarkdownExporter()
+    # # Convert the notebook to RST format
+    # (body, resources) = md_exporter.from_filename(nb_filename)
+    # print(f"::debug::Saving{md_filename}")
 
-    with open(md_filename, 'w') as f:
-        f.write(body)
+    # with open(md_filename, 'w') as f:
+    #     f.write(body)
 
-    print(f"::set-output name=state::Converted {nb_filename} to {md_filename}")
+    print(f"::set-output name=result::Converted {nb_filename} to {md_filename}")
 
 if __name__ == '__main__':
     main()
